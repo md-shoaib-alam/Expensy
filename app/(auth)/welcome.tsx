689d8ac13cd415b3875c15b48127051c41f5de35
md-shoaib-alam <shoaibsalam0786@gmail.com>
2025-08-22 23:29:26 +0530

    added animation in welcome screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -5,6 +5,7 @@ import Typo from "@/components/Typo";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
 import Button from "@/components/Button";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 const Welcome = () => {
   return (
     <ScreenWrapper>
@@ -14,7 +15,8 @@ const Welcome = () => {
           <TouchableOpacity style={styles.loginButton}>
             <Typo fontWeight={"500"}>Sign in</Typo>
           </TouchableOpacity>
-          <Image
+          <Animated.Image
+            entering={FadeIn.duration(1000)}
             source={require("../../assets/images/welcome.png")}
             style={styles.WelcomeImage}
             resizeMode="contain"
@@ -22,20 +24,40 @@ const Welcome = () => {
         </View>
         {/* footer */}
         <View style={styles.footer}>
-          <View style={{ alignItems: "center" }}>
-            <Typo size={30} fontWeight={"800"}> Always Take Control</Typo>
-            <Typo size={30} fontWeight={"800"}> Of Your Finance </Typo>
-          </View>
-          <View style={{alignItems:"center", gap:2}}>
-             <Typo size={17} color={colors.textLight}> Finance must be arranged to set  a better </Typo>
-            <Typo size={17} color={colors.textLight} > Lifestyle in future </Typo>
-          </View>
-          <View style={styles.buttonContainer}>
-{/* button */}
-<Button>
-  <Typo size={22} color={colors.neutral900} fontWeight={"600"}>Get Started</Typo>
-</Button>
-          </View>
+          <Animated.View
+            entering={FadeInDown.duration(1000).springify().damping(12)}
+            style={{ alignItems: "center" }}
+          >
+            <Typo size={30} fontWeight={"800"}>
+              {" "}
+              Always Take Control
+            </Typo>
+            <Typo size={30} fontWeight={"800"}>
+              {" "}
+              Of Your Finance{" "}
+            </Typo>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.duration(1000).delay(100).springify().damping(12)}
+            style={{ alignItems: "center", gap: 2 }}
+          >
+            <Typo size={17} color={colors.textLight}>
+              {" "}
+              Finance must be arranged to set a better{" "}
+            </Typo>
+            <Typo size={17} color={colors.textLight}>
+              {" "}
+              Lifestyle in future{" "}
+            </Typo>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.duration(1000).delay(100).springify().damping(12)} style={styles.buttonContainer}>
+            {/* button */}
+            <Button>
+              <Typo size={22} color={colors.neutral900} fontWeight={"600"}>
+                Get Started
+              </Typo>
+            </Button>
+          </Animated.View>
         </View>
       </View>
     </ScreenWrapper>
