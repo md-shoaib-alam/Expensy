30f657c2253f8dd1006a0725e9369869c3807d02
md-shoaib-alam <shoaibsalam0786@gmail.com>
2025-08-22 23:15:28 +0530

    wlecome page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-
+import { Slot } from 'expo-router'
 const _layout = () => {
   return ( <Stack screenOptions={{headerShown:false}}></Stack>
   )
---
+++ components/Typo.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { TextStyle } from "react-native";
+import { TypoProps } from "@/types";
+const Typo = ({
+  size,
+  color = colors.text,
+  fontWeight = "400",
+  children,
+  style,
+  textProps = {},
+}: TypoProps) => {
+  const textStyle: TextStyle = {
+    fontSize: size ? verticalScale(size) : verticalScale(18),
+    color,
+    fontWeight,
+  };
+  return (
+    <Text style={[textStyle, style]} {...textProps}>
+      {children}
+    </Text>
+  );
+};
+
+export default Typo;
+
+const styles = StyleSheet.create({});
